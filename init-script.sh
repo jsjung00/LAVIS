80fe97b401ca18c6ddc6092a3e48373b21423a4b
sguntury <sguntury@chanzuckerberg.com>
2025-08-19 16:24:36 -0700

    more changes to make it run

--- init-script.sh
+++ init-script.sh
@@ -4,18 +4,18 @@ sudo apt-get update && sudo apt-get install -y libgl1 vim openjdk-11-jre
 
 uv venv --python=3.8
 
-source /flow/.venv/bin/activate
+source /work/.venv/bin/activate
 
 DATA_TGZ_FILE="/scratch/lten.tgz"
 
-if [ -f "$DATA_TGZ_FILE" ]; then
-if [ ! -d "$DIR" ]; then
-  echo "Error: Directory $DIR does not exist." >&2
+if [ ! -f "$DATA_TGZ_FILE" ]; then
+  echo "Error: file $DATA_TGZ_FILE does not exist." >&2
   exit 1
 fi
 
-#tar -zxvf /scratch/lfull.tgz -C /flow/LAVIS
-tar -zxvf /scratch/lten.tgz -C /flow/LAVIS_10
+mkdir -p /flow/LAVIS_10
 
+#tar -zxvf /scratch/lfull.tgz -C /flow/LAVIS
+tar -zxvf /scratch/lten.tgz -C /flow/
 
 echo "------ INITIALIZATION IS DONE --------"
