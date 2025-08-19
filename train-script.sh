5937a4ccb2637b23710945b37f935b010c0f2dc0
Sashidhar Guntury <sguntury@chanzuckerberg.com>
2025-08-19 16:09:06 -0700

    add the dist script as well

--- train-script.sh
+++ train-script.sh
@@ -4,4 +4,11 @@ cd /work
 uv venv
 source .venv/bin/activate
 
-torchrun --standalone --nprocs_per_node=gpu train.py
+# single node training
+torchrun \
+  --standalone \
+  --nprocs_per_node=gpu \
+  train.py --cfg-path lavis/projects/blip2/train/caption_coco_ft.yaml
+
+# multinode training
+# torchrun --nproc_per_node=8 --nnodes=3 train.py --cfg-path lavis/projects/blip2/train/caption_coco_ft.yaml
