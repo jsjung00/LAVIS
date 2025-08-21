e8cbb81e21612f1593d71444e0ed45e83bf2142c
sguntury <sguntury@chanzuckerberg.com>
2025-08-21 10:10:39 -0700

    do not try to copy data

--- init-script.sh
+++ init-script.sh
@@ -8,14 +8,14 @@ source /work/.venv/bin/activate
 
 uv pip install -e .
 
-DATA_TGZ_FILE="/scratch/lfull.tgz"
+#DATA_TGZ_FILE="/scratch/lfull.tgz"
 
-if [ ! -f "$DATA_TGZ_FILE" ]; then
-  echo "Error: file $DATA_TGZ_FILE does not exist." >&2
-  exit 1
-fi
+#if [ ! -f "$DATA_TGZ_FILE" ]; then
+#  echo "Error: file $DATA_TGZ_FILE does not exist." >&2
+#  exit 1
+#fi
 
-tar -zxvf /scratch/lfull.tgz -C /flow/
+#tar -zxvf /scratch/lfull.tgz -C /flow/
 #tar -zxvf /scratch/lten.tgz -C /flow/
 
 echo "------ INITIALIZATION IS DONE --------"
